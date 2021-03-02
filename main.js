79ae7a5c6ed4185af0ba6e3d031865ab40426131
Gauhar0404 <gauharkystabayeva@gmail.com>
2021-03-02 13:22:25 -0800

    added html and javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+var myName = "Gauhar"
+var numberOfKids = 3;
+var partnerName = "Lucky";
+var cityName = "San Diego";
+var jobTitle ="Front-End Developer";
+var myFortune = "You will be a" + jobTitle + " in " + cityName + " and married to " + partnerName + " with " + numberOfKids + " kids ";
+console.log (myFortune)
